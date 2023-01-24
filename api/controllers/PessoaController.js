b543a81ec287774d54d456ef77664b3b0eec6010
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-01-24 20:59:06 -0300

    Controles e rotas relacionadas a matrículas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -52,6 +52,34 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+
+    // /pessoas/:estudanteId/matricula/:matriculaId
+    static async buscaUmaMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        try {
+            const umaMatricula = await database.Matriculas.findOne(
+                { where: 
+                    {
+                        id: matriculaId, 
+                        estudante_id: estudanteId 
+                    } 
+                })
+            return res.status(200).json(umaMatricula)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async criaMatricula(req, res) {
+        const { estudanteId } = req.params
+        const novaMatricula = { ...req.body, estudante_id: Number(estudanteId) }
+        try {
+            const novaMatriculaCriada = await database.Matriculas.create(novaMatricula)
+            return res.status(200).json(novaMatriculaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,5 +8,7 @@ router.get('/pessoas/:id', PessoaController.buscaPorId)
 router.post('/pessoas', PessoaController.criaPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoaController.excluiPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.buscaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
 
 module.exports = router
